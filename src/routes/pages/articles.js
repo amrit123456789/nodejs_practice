943d8a912e4516a0c59184e8d3ae181f5505e22c
amrit123456789 <amritagarwal98@gmail.com>
2019-01-19 15:25:54 +0530

    image not loading
    Signed-off-by: amrit123456789 <amritagarwal98@gmail.com>

--- src/routes/pages/articles.js
+++ src/routes/pages/articles.js
@@ -1,7 +1,7 @@
 const route = require('express').Router()
 const { fetchArticles, fetchArticleById } = require('../../controllers/articles')
 
-route.get('/', async (req, res) => {
+route.get('', async (req, res) => {
   try {
     const articles = await fetchArticles()
     res.render('pages/articles', { articles })
